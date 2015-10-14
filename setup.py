a7cb2f72d0fa4bd574754341a258fe5f35ca56c6
AboorvaDevarajan <aburvadevarajan@gmail.com>
2015-10-14 22:01:16 +0530

    upadted

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='vmresourcealloc',
+      version='1.0',
+      description='OpenShift App',
+      author='Aboorva Devarajan',
+      author_email='aburvadevarajan@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['flask','matplotlib','numpy'],
+     )
